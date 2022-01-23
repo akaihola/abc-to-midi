77e6cf854d432756ef6973e979c96bf856c997db
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2022-01-23 20:53:28 +0200

    Add test case with chords

--- tests/abc2midi_parity.rs
+++ tests/abc2midi_parity.rs
@@ -1,17 +1,20 @@
+#[cfg(test)]
 use abc_parser::abc;
 use abc_to_midi::midly_wrappers::Smf;
 use pretty_assertions::assert_eq;
+use rstest::rstest;
 
-
-#[cfg(test)]
-
-#[test]
-fn compare() {
-    let abc2midi_raw = std::fs::read("test-asset/three-quarters.mid").unwrap();
+#[rstest(
+    name,
+    case::three_quarters("three-quarters"),
+    case::chord("chord"),
+)]
+fn compare(name: &str) {
+    let abc2midi_raw = std::fs::read(format!("test-asset/{}.mid", name)).unwrap();
     let abc2midi_output = midly::Smf::parse(&abc2midi_raw).unwrap();
 
-    let abc_example = std::fs::read_to_string("test-asset/three-quarters.abc").unwrap();
-    let abc_parsed = abc::tune(&abc_example).unwrap();
+    let abc_example = std::fs::read_to_string(format!("test-asset/{}.abc", name)).unwrap();
+    let abc_parsed = &abc::tune(&abc_example).unwrap();
     let Smf(abc_to_midi_output) = abc_parsed.try_into().unwrap();
 
     assert_eq!(abc_to_midi_output, abc2midi_output);
